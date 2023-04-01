c92daa03d91b86bf5efc06cff8d32d032fff4262
Roland Sebestyen <roland@Rolands-MacBook-Air.local>
2023-04-01 16:31:49 +0300

    Refactored to use RouteFragment class

--- lib/index.ts
+++ lib/index.ts
@@ -3,95 +3,155 @@ const RouteMethods = {
   POST: "POST",
   PUT: "PUT",
   DELETE: "DELETE",
+  PATCH: "PATCH",
 } as const;
 
 type RouteMethod = keyof typeof RouteMethods;
 
-type RouteMiddleware = () => void;
+type RouterHandlerResponse = void;
 
-type RouteHandler = () => void;
+type RouteMiddleware = () => RouterHandlerResponse;
 
-type RouteHandlers = {
-  GET?: RouteHandler;
-  POST?: RouteHandler;
-  PUT?: RouteHandler;
-  DELETE?: RouteHandler;
-};
+type RouteHandler = () => RouterHandlerResponse;
 
-type RouteFragmenet = {
-  fragment: string;
-  middlewares: RouteMiddleware[];
-  handlers: RouteHandlers;
-  children: RouteFragmenet[];
+export type RouteFragmentOptions = {
   parameterName?: string;
 };
 
-const createRouteFragment = (fragment: string): RouteFragmenet => {
-  return {
-    fragment,
-    middlewares: [],
-    handlers: {},
-    children: [],
-  };
-};
+class RouteFragmenet {
+  fragment: string;
+  middlewares: RouteMiddleware[] = [];
+  handlers: Map<RouteMethod, RouteHandler> = new Map();
+  children: RouteFragmenet[] = [];
+  private parameterName?: string;
 
-const PARAMETER_FRAGMENT_NAME = "$$PARAM$$";
+  constructor(fragment: string, options?: RouteFragmentOptions) {
+    this.fragment = fragment;
+    this.parameterName = options?.parameterName;
+  }
+
+  getParameterName() {
+    return this.parameterName;
+  }
+
+  get(): RouterHandlerResponse {
+    return this.handleMethod(RouteMethods.GET);
+  }
+
+  post(): RouterHandlerResponse {
+    return this.handleMethod(RouteMethods.POST);
+  }
+
+  put(): RouterHandlerResponse {
+    return this.handleMethod(RouteMethods.PUT);
+  }
+
+  delete(): RouterHandlerResponse {
+    return this.handleMethod(RouteMethods.DELETE);
+  }
+
+  patch(): RouterHandlerResponse {
+    return this.handleMethod(RouteMethods.PATCH);
+  }
+
+  private handleMethod(method: RouteMethod): RouterHandlerResponse {
+    console.log(`Will call get for the ${this.fragment}`);
+    if (!this.handlers.has(method)) {
+      console.error(`Usuported method type ${method} for ${this.fragment}`);
+      return;
+    }
+
+    const handler = this.handlers.get(method);
+    // @TODO when will integrate will node server
+    return handler.apply(this);
+  }
+}
+
+const createRouteFragment = (fragment: string): RouteFragmenet => {
+  return new RouteFragmenet(fragment);
+};
 
 class Router {
   __ROUTER__: RouteFragmenet;
 
+  private PARAMETER_FRAGMENT_NAME = "$$PARAM$$";
+
   constructor() {
-    this.__ROUTER__ = this.createRouteFragment("/");
+    this.__ROUTER__ = new RouteFragmenet("/");
   }
 
   insertHandler(route: string, method: RouteMethod, handler: RouteHandler) {
-    const fragmenet = this.buildRoute(route);
+    const fragmenet = this.findOrCreateRouteFragment(route);
 
-    if (fragmenet.handlers[method]) {
+    if (fragmenet.handlers.has(method)) {
       throw new Error(`Route ${route} with method ${method} is already used`);
     }
 
-    fragmenet.handlers[method] = handler;
+    fragmenet.handlers.set(method, handler);
   }
 
-  private buildRoute(route: string): RouteFragmenet {
-    const parts = route.split("/");
-    if (route.startsWith("/")) {
-      parts.shift();
-    }
+  private findOrCreateRouteFragment(route: string): RouteFragmenet {
+    const parts = this.getRouteParts(route);
 
     let currentFragment = this.__ROUTER__;
 
     for (let part of parts) {
-      const isParameter = part.startsWith(":");
-      const fragmentName = isParameter ? PARAMETER_FRAGMENT_NAME : part;
-
-      const fragment = currentFragment.children.find(
-        ({ fragment }) => fragment === fragmentName
+      const routeFragment = this.findChildrenFragment(
+        currentFragment,
+        this.extractFramgentNameFromPart(part)
       );
 
-      if (fragment) {
-        currentFragment = fragment;
+      if (routeFragment) {
+        currentFragment = routeFragment;
       } else {
-        const fragment = createRouteFragment(fragmentName);
+        const fragment = this.createRouteFragmentFromPart(part);
         currentFragment.children.push(fragment);
         currentFragment = fragment;
-        if (isParameter) {
-          fragment.parameterName = part.replace(":", "");
-        }
       }
     }
 
     return currentFragment;
   }
 
-  private createRouteFragment(fragment: string): RouteFragmenet {
-    return {
-      fragment,
-      middlewares: [],
-      handlers: {},
-      children: [],
-    };
+  private findChildrenFragment(
+    routeFragment: RouteFragmenet,
+    fragmentName: string
+  ): RouteFragmenet | null {
+    for (const currentRouteFragment of routeFragment.children) {
+      if (currentRouteFragment.fragment === fragmentName) {
+        return currentRouteFragment;
+      }
+    }
+
+    return null;
+  }
+
+  private getRouteParts(route: string): string[] {
+    const parts = route.split("/");
+    if (route.startsWith("/")) {
+      parts.shift();
+    }
+
+    return parts;
+  }
+
+  private isParameterPart(part: string) {
+    return part.startsWith(":");
+  }
+
+  private extractFramgentNameFromPart(part: string) {
+    return this.isParameterPart(part) ? this.PARAMETER_FRAGMENT_NAME : part;
+  }
+
+  private extractParameterNameFromPart(part: string) {
+    return part.replace(":", "");
+  }
+
+  private createRouteFragmentFromPart(part: string): RouteFragmenet {
+    const options: RouteFragmentOptions = this.isParameterPart(part)
+      ? { parameterName: this.extractParameterNameFromPart(part) }
+      : {};
+    return new RouteFragmenet(this.extractFramgentNameFromPart(part), options);
   }
 }
 
